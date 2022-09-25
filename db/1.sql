39cc555ace30fe8688f98afadebfc27d3cf3fc62
William <santifon15@gmail.com>
2022-09-25 13:41:17 -0500

    add database

---
+++ db/1.sql
@@ -0,0 +1,36 @@
+-- init data base (Postgres)
+
+create table genre(
+    id serial primary key,
+    name varchar(50) not null unique,
+    image varchar
+);
+
+create table movie(
+    id serial primary key,
+    image varchar,
+    title varchar(100) not null,
+    creation_date date not null,
+    calification int not null
+);
+
+create table movie_genre(
+    id serial primary key,
+    movie integer references movie(id),
+    genre integer references genre(id)
+);
+
+create table character(
+    id serial primary key,
+    image varchar,
+    name varchar(100) not null,
+    age int,
+    weight real,
+    history varchar
+);
+
+create table character_movie(
+    id serial primary key,
+    actor integer references actor(id),
+    movie integer references movie(id)
+);
