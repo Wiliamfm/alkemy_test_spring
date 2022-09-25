f1df93d75097cba720dc8c4724987a183f4fd3ac
William <santifon15@gmail.com>
2022-09-25 09:10:17 -0500

    initial

---
+++ src/main/java/alkemy/test/alkemy/test/AlkemyTestApplication.java
@@ -0,0 +1,13 @@
+package alkemy.test.alkemy.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkemyTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkemyTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alkemy/test/alkemy/test/AlkemyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package alkemy.test.alkemy.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkemyTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
